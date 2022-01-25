ad5f0262e84e6286d066a9e94cc1c2ea3f27ce4f
karelvanoordt <63868555+karelvanoordt@users.noreply.github.com>
2022-01-25 03:08:43 -0500

    Add first classes

---
+++ student.rb
@@ -0,0 +1,13 @@
+require relative 'person'
+
+class Student < Person
+  def initialize(classroom, age, parent_permission: true, name = 'Unknown')
+    super(age, name, parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
+  
+end
